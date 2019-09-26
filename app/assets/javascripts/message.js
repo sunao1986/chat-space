96f0e55a65dd8cb6d0cdde7f17bcfc0782eef227
sunao1986 <nk@sunao-products.com>
2019-09-26 21:32:40 +0900

    Asynchronous implementation

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>            
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    // console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(date){  //引数はdate
+      // console.log(message.content);ここでjbilderは成功している
+      var html = buildHTML(date);
+      $('.messages').append(html)
+      $('#new_message').val('')
+      // $().メッセージをスクロールする.animate
+      // $('sendボタンのセレクタ').ボタンが復活するメソッド
+    })
+    .fail(function() {
+      alert('送信に失敗しました');
+    })
+  })
+})
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,15 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # binding.pry ここで発火してJSONが送られてることを確認済み
+    #Jsonで欲しいのは@messageに入ってるcontent
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|       
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -20,10 +26,11 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image, :created_at).merge(user_id: current_user.id)
   end
 
   def set_group
     @group = Group.find(params[:group_id])
   end
 end
+

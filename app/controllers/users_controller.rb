15883d56111e092edef51eaeb60bf5f72f6b20c2
sunao1986 <nk@sunao-products.com>
2019-10-01 17:46:44 +0900

    search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.where('name LIKE(?) AND id !=?',"%#{params[:keyword]}%", current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
   end
 
   def update
-    if current_user.update(user_params) #アップデートに成功したらrootへ
+    if current_user.update(user_params) 
       redirect_to root_path
     else
-      render :edit #失敗したら編集画面に戻る
+      render :edit 
     end
   end
 
